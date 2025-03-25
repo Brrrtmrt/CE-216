1dcf7d4f6308f34a85145b0598f624c14fbcaf88
Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>
2025-03-25 21:51:28 +0300

    delete

--- src/main/java/com/ce216/Draw.java
+++
@@ -1,7 +0,0 @@
-package com.ce216;
-
-public class Draw { //class for JavaFX
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/ce216/DrawController.java
+++
@@ -1,5 +0,0 @@
-package com.ce216;
-
-public class DrawController {
-
-}
