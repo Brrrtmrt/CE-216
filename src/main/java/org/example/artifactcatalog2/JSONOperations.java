8d56df6dd7e31d447828e7ff62f10b0b6d7ba088
Brrrtmrt <mert.saadet@std.izmirekonomi.edu.tr>
2025-03-28 22:53:30 +0300

    Added serializing and deserializing and type adapters.
    Signed-off-by: Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>

--- src/main/java/org/example/artifactcatalog2/JSONOperations.java
+++ src/main/java/org/example/artifactcatalog2/JSONOperations.java
@@ -8,22 +8,33 @@ import java.util.Arrays;
 
 public class JSONOperations {
 
-
-
     public static void main(String[] args) {
         //TEST object
         Artifact art=new Artifact("Foo","Bar", "ManuScript","İzmir",new ArrayList<>(Arrays.asList("Test","MS")),"A", LocalDate.of(2025,10,10),"İzmir",new Dimension(10,10,10),10000,new ArrayList<>(Arrays.asList("MS","value")));
 
         //Serializing process
+        JSONOperations jsonObj = new JSONOperations();
+        String res = jsonObj.serialize(art);
+        System.out.println(res);
+
+        //Deserializing process
+        Artifact a = jsonObj.deserialize(res);
+        System.out.println(a);
+
+    }
+
+    public String serialize(Artifact artifact) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
                 .create();
-        String json = gson.toJson(art);
-        System.out.println(json);
+        return gson.toJson(artifact);
 
-        //Deserializing process
-        Artifact deserialized = gson.fromJson(json, Artifact.class);
-        System.out.println(deserialized);
+    }
 
+    public Artifact deserialize(String json) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
+                .create();
+        return gson.fromJson(json, Artifact.class);
     }
 }
