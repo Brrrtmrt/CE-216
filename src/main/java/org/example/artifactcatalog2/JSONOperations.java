2da17113e123ebdebc861e5eeafcfb9a4fcaba1c
Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>
2025-03-29 13:26:24 +0300

    Update JSON methods, Added writing to JSON file.
    Signed-off-by: Brrrtmrt <69765218+Brrrtmrt@users.noreply.github.com>

--- src/main/java/org/example/artifactcatalog2/JSONOperations.java
+++ src/main/java/org/example/artifactcatalog2/JSONOperations.java
@@ -1,29 +1,52 @@
 package org.example.artifactcatalog2;
 
 import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
-import java.time.LocalDate;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class JSONOperations {
 
-    public static void main(String[] args) {
-        //TEST object
-        Artifact art=new Artifact("Foo","Bar", "ManuScript","İzmir",new ArrayList<>(Arrays.asList("Test","MS")),"A", LocalDate.of(2025,10,10),"İzmir",new Dimension(10,10,10),10000,new ArrayList<>(Arrays.asList("MS","value")));
+    public static boolean importJSON(Path file) throws IOException {
 
-        //Serializing process
-        JSONOperations jsonObj = new JSONOperations();
-        String res = jsonObj.serialize(art);
-        System.out.println(res);
-
-        //Deserializing process
-        Artifact a = jsonObj.deserialize(res);
-        System.out.println(a);
-
+        return false;
     }
 
-    public String serialize(Artifact artifact) {
+    public static boolean exportJSON(Path in, Path out) throws IOException {
+
+        return false;
+    }
+
+    //Main JSON File for programs usage
+    public static boolean writeJSON(Path path, ArrayList<Artifact> list) {
+        File out = path.toFile();
+        try (FileWriter fw = new FileWriter(out)) {
+            String json = serialize(list);
+            fw.write(json);
+            fw.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        //TEST object
+       /* Artifact art = new Artifact("Foo", "Bar", "ManuScript", "İzmir", new ArrayList<>(Arrays.asList("Test", "MS")), "A", LocalDate.of(2025, 10, 10), "İzmir", new Dimension(10, 10, 10), 10000, new ArrayList<>(Arrays.asList("MS", "value")));
+        Artifact art2 = new Artifact("test", "test", "ManuScript", "İzmir", new ArrayList<>(Arrays.asList("Test", "MS")), "A", LocalDate.of(2025, 10, 10), "İzmir", new Dimension(10, 10, 10), 10000, new ArrayList<>(Arrays.asList("MS", "value")));
+        Artifact art3 = new Artifact("asddsa", "adsdas", "ManuScript", "İzmir", new ArrayList<>(Arrays.asList("Test", "MS")), "A", LocalDate.of(2025, 10, 10), "İzmir", new Dimension(10, 10, 10), 10000, new ArrayList<>(Arrays.asList("MS", "value")));
+        */
+    }
+
+    @Deprecated
+    public static String serialize(Artifact artifact) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
                 .create();
@@ -31,10 +54,33 @@ public class JSONOperations {
 
     }
 
-    public Artifact deserialize(String json) {
+    public static String serialize(ArrayList<Artifact> artifact) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
+                .create();
+        return gson.toJson(artifact);
+
+    }
+
+    @Deprecated
+    public static Artifact deserialize(String json) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
                 .create();
         return gson.fromJson(json, Artifact.class);
     }
+
+    public static ArrayList<Artifact> deserializeList(String json) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Artifact.class, new ArtifactTypeAdapter()).registerTypeAdapter(Dimension.class, new DimensionTypeAdapter())
+                .create();
+        Type artifactListType = new TypeToken<ArrayList<Artifact>>() {
+        }.getType();
+        return gson.fromJson(json, artifactListType);
+    }
+
+
+
+
+
 }
