740b7b4bab64967ad953d97ca524c8cd784c7e46
mertutmaa <mertutma4@outlook.com>
2025-03-31 22:33:20 +0300

    more widgets at the main page
    I am currently using anchor pane as the container but having some issues with it. Gonna try to change to VBox/HBox method. Before that wanted to save current state.

--- src/main/java/org/example/artifactcatalog2/ControllerMain.java
+++ src/main/java/org/example/artifactcatalog2/ControllerMain.java
@@ -2,18 +2,32 @@ package org.example.artifactcatalog2;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerMain implements Initializable {
     @FXML
-    private ChoiceBox<Button> myChoiceBox;
+    private ChoiceBox<String> filter;
+    @FXML
+    private Label mainLabel;
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private VBox mainLayout;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
-
+        filter.setValue("Filter");
+        HBox firsLine = new HBox();
+        firsLine.setAlignment(Pos.CENTER);
+        
     }
 }
