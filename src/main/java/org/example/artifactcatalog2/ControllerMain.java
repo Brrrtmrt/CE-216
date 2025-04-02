287b2d3fa2220b3ae0a24bb391beccaffd5cd07a
mertutmaa <mertutma4@outlook.com>
2025-04-02 15:27:31 +0300

    Page layout for unique items and some little changes
    -Dark mode can be closed now
    -Searched items will be on list view but there is an unwanted gap between search bar and list view i will take care of it later.
    -Couple of changes in UI
    -Search button sends user to the unique page dedicated to the selected item

--- src/main/java/org/example/artifactcatalog2/ControllerMain.java
+++ src/main/java/org/example/artifactcatalog2/ControllerMain.java
@@ -2,19 +2,23 @@ package org.example.artifactcatalog2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerMain implements Initializable {
     @FXML
-    private ChoiceBox<String> filter;
+    private ChoiceBox<String> filter;   //filter choice box
     @FXML
     private Label mainLabel;
     @FXML
@@ -23,6 +27,10 @@ public class ControllerMain implements Initializable {
     private VBox mainLayout;
     @FXML
     private CheckMenuItem darkModeChecker;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private ListView<String> myListResults;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
@@ -30,12 +38,34 @@ public class ControllerMain implements Initializable {
         HBox firsLine = new HBox();
         firsLine.setAlignment(Pos.CENTER);
 
+
+        String[] forTest = {"aasd", "asdas", "dfdf", "szfsdfds"};
+        myListResults.getItems().addAll(forTest);
+
     }
     public void darkMode(ActionEvent event){
         String darkModeCSS = this.getClass().getResource("DarkMode.css").toExternalForm();
         if(darkModeChecker.isSelected()){
             Scene sceneMain = darkModeChecker.getParentPopup().getOwnerWindow().getScene();
             sceneMain.getStylesheets().add(darkModeCSS);
+        }
+        else{
+            Scene sceneMain = darkModeChecker.getParentPopup().getOwnerWindow().getScene();
+            sceneMain.getStylesheets().remove(darkModeCSS);
+        }
+    }
+
+    public void search(ActionEvent event){
+        //method that gets activated when user press search button
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("uniquePage.fxml"));
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }
---
+++ src/main/java/org/example/artifactcatalog2/ControllerUnique.java
@@ -0,0 +1,24 @@
+package org.example.artifactcatalog2;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerUnique implements Initializable {
+    @FXML
+    private CheckMenuItem darkModeChecker;
+    @FXML
+    private ImageView pictureArtifact;
+    @FXML
+    private Label titleArtifact;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
